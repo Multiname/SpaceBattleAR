d5ccd9a513eef80b513377fc6acfe2cf94882d15
Multiname <MineFend@gmail.com>
2025-04-01 18:44:05 +0300

    Добавлено получение информации о корабле на поле

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Spaceship : MonoBehaviour
 {
     [SerializeField]
     private MeshRenderer meshRenderer;
+    [SerializeField]
+    private Image cardImage;
+    private CardInfoContainer cardInfoContainer;
 
     [HideInInspector]
     public int playerIndex = 0;
 
+    private float holdDuration = 1.0f;
+    private float currentHoldDuration = 0.0f;
+
     private void Start() {
         meshRenderer.material.color = Color.green;
+        cardInfoContainer = GameObject.FindGameObjectWithTag("CardInfoContainer").GetComponent<CardInfoContainer>();
     }
 
     public void SetFriendliness(bool friendly) {
@@ -18,5 +27,23 @@ public class Spaceship : MonoBehaviour
         } else {
             meshRenderer.material.color = Color.red;
         }
+    }
+
+    private void OnMouseDrag() {
+        if(!EventSystem.current.IsPointerOverGameObject()) {
+            currentHoldDuration += Time.deltaTime;
+            if (currentHoldDuration >= holdDuration) {
+                currentHoldDuration = 0.0f;
+                cardInfoContainer.ShowCardInfo(cardImage.sprite);
+            }
+        }
+    }
+
+    private void OnMouseExit() {
+        currentHoldDuration = 0.0f;
+    }
+
+    private void OnMouseUp() {
+        currentHoldDuration = 0.0f;
     }
 }

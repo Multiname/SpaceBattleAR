e46d0d6f429f6cc691d956fff0466e8e3f0f8d12
Multiname <MineFend@gmail.com>
2025-05-01 15:01:21 +0300

    Исправлены модели Сокола и СИД-истребителя

--- Assets/Scripts/Menu/MenuSpaceshipLauncher.cs
+++ Assets/Scripts/Menu/MenuSpaceshipLauncher.cs
@@ -7,9 +7,6 @@ public class MenuSpaceshipLauncher : MonoBehaviour
     [SerializeField]
     private List<GameObject> spaceships = new();
     [SerializeField]
-    [Range(0, 1)]
-    private List<float> spaceshipSizes = new();
-    [SerializeField]
     private GameObject spaceshipDriver;
     [SerializeField]
     private float cooldown = 5.0f;
@@ -27,7 +24,6 @@ public class MenuSpaceshipLauncher : MonoBehaviour
         spaceship.transform.SetParent(driver.transform);
         spaceship.transform.localPosition = Vector3.zero;
         spaceship.transform.localRotation = Quaternion.Euler(Vector3.zero);
-        spaceship.transform.localScale = Vector3.one * spaceshipSizes[currentIndex];
 
         ++currentIndex;
         if (currentIndex >= spaceships.Count) {
